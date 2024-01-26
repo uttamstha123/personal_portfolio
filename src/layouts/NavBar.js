44832de217aa12aa82e3ab820627ef845f3ff2dc
Uttam Shrestha <shresthauttam66@gmail.com>
2024-01-26 14:21:52 +0530

    first commit

---
+++ src/layouts/NavBar.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Button from "react-bootstrap/Button";
+// Custom CSS
+import "./nav.css";
+
+const NavBar = () => {
+  return (
+    <div className="navbar p-0 ">
+      <Navbar collapseOnSelect expand="lg" className="padding-x w-100">
+        <Container className="m-0 p-0 w-100">
+          <Navbar.Brand className="fs-xxl navTitle" href="/">
+            Res<span>ume.</span>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="ms-auto align-items-center gap-5 py-3 py-md-0">
+              <Nav.Link className="fs-sm" href="#skills">
+                Skills
+              </Nav.Link>
+              <Nav.Link className="fs-sm" href="#projects">
+                Projects
+              </Nav.Link>
+              <Nav.Link className="fs-sm" href="#experience">
+                Experience
+              </Nav.Link>
+              <Nav.Link href="#memes">
+                <Button className="fs-sm primaryCtn pe-3">Get In Touch</Button>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
+
+export default NavBar;
---
+++ src/section/Header.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./style.css";
+
+const Header = () => {
+  return (
+    <div className="row m-0 align-items-center header padding-x">
+      <div className="col-12 col-md-7 text-center py-5 py-md-0 text-md-start">
+        <h1 className="fs-xxxl profileName">
+          UTTAM <br />
+          SHRESTHA
+        </h1>
+        <div className="profile_desc col-md-10 fs-md">
+          <p className="m-0 mt-sm-5">Hi,</p>
+          <p>
+            I am an experienced website developer with a passion who's all about
+            turning ideas into websites that just work. In my portfolio, you'll
+            find a mix of projects that showcase my love for clean design and
+            smooth functionality.
+          </p>
+        </div>
+      </div>
+      <div className="col-12 col-md-5 text-end">
+        <div className="text-center">
+          <img className="profileImg" src="./profile_img.png" width={"90%"} />
+          <h2 className="fs-xl mt-3 profession">Web Developer</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
